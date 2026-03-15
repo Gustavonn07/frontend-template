cc74c2b2ba2743b376ae19edbf6c495b4884c420
Gustavonn07 <gustavonepnog@gmail.com>
2026-03-15 14:55:16 -0300

    fix: testes

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,35 +7,41 @@ const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(file
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './vitest.setup.ts',
-    projects: [{
-      extends: true,
-      plugins: [
-      // The plugin will run tests for the stories defined in your Storybook config
-      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-      storybookTest({
-        configDir: path.join(dirname, '.storybook')
-      })],
-      test: {
-        name: 'storybook',
-        browser: {
-          enabled: true,
-          headless: true,
-          provider: playwright({}),
-          instances: [{
-            browser: 'chromium'
-          }]
-        },
-        setupFiles: ['.storybook/vitest.setup.ts']
-      }
-    }]
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname)
     }
-  }
-});
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './vitest.setup.ts'
+  },
+  projects: [
+    {
+      test: {
+        name: 'unit',
+        include: ['**/*.test.ts', '**/*.test.tsx']
+      }
+    },
+    {
+      extends: true,
+      plugins: [
+        storybookTest({
+          configDir: path.join(dirname, '.storybook')
+        })
+      ],
+      test: {
+        name: 'storybook',
+        include: ['**/*.stories.ts', '**/*.stories.tsx'],
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: playwright({}),
+          instances: [{ browser: 'chromium' }]
+        },
+        setupFiles: ['.storybook/vitest.setup.ts']
+      }
+    }
+  ]
+})
